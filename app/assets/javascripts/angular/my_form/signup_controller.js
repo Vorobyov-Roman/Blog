e41692f35810df2300d7da5f74056bf83fd2c1aa
Vorobyov-Roman <vorobrawr@gmail.com>
2016-04-25 00:16:45 +0300

    form gets cleared when the dialog closes

--- app/assets/javascripts/angular/my_form/signup_controller.js
+++ app/assets/javascripts/angular/my_form/signup_controller.js
@@ -1,14 +1,19 @@
 (function() {
   'use strict';
 
-  function signupController(authService, $rootScope) {
+  function signupController(authService, $scope, $rootScope) {
     var self = this;
     var dialog = $('#signup');
 
+    function closeDialog() {
+      dialog.modal('hide').on('hidden.bs.modal', function(e) {
+        self.userinfo = null;
+        $scope.form.$setPristine();
+      });
+    }
+
     self.submit = function() {
       function onSuccess(data) {
-        console.log('success', data);
-
         var credentials = {
           name: self.userinfo.name,
           password: self.userinfo.password
@@ -16,7 +21,7 @@
 
         authService.logIn(credentials).then(function(token) {
           $rootScope.$broadcast('LOGGED_IN', token);
-          dialog.modal('hide');
+          closeDialog();
         });
       }
 
@@ -24,12 +29,25 @@
         console.log('error', error);
       }
 
-      authService.register(self.userinfo).then(onSuccess, onError);
+      angular.forEach($scope.form, function(field, key) {
+        if (key[0] !== '$') {
+          field.$setDirty();
+        }
+      });
+
+      if ($scope.form.$valid) {
+        authService.register(self.userinfo).then(onSuccess, onError);
+      }
+    }
+
+    self.close = function() {
+      closeDialog();
     }
   }
 
   angular.module('myForm').controller('SignupController', [
     'authService',
+    '$scope',
     '$rootScope',
     signupController
   ]);
